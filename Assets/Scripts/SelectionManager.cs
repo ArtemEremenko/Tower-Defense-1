067e0b7af19db8361c69c40279e5828bf4dabb43
EremenkoArtem <Eremenko_Artem@i.ua>
2024-06-04 22:40:37 +0300

    SelectTower() method first iteration

--- Assets/Scripts/SelectionManager.cs
+++ Assets/Scripts/SelectionManager.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class SelectionManager : MonoBehaviour
 {
     void Update()
     {
-        RaycastHit hit;
         if (Input.GetMouseButtonDown(0))
         {
             SelectTower();
@@ -16,5 +16,20 @@ public class SelectionManager : MonoBehaviour
     private void SelectTower()
     {
         Debug.Log("Trying to Select some...");
+
+        Vector2 mousePosition = Input.mousePosition;
+
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction); //whre origin is a camera pos and direction is a mouse position
+
+        if (hit.collider)
+        {
+            Debug.Log("and it is not null");
+        }
+        else 
+        {
+            Debug.Log("nothing to select");
+        }
     }
 }
